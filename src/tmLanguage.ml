4e9b12414f52075b002b9409afa5a176a10adda1
Alan Hu <alanh@ccs.neu.edu>
2021-04-08 23:38:57 -0400

    Split the code into multiple files

--- src/tmLanguage.ml
+++ src/tmLanguage.ml
@@ -1,235 +1,5 @@
-module IntMap = Map.Make(Int)
-
-type capture = {
-  capture_name : string option;
-  capture_patterns : rule list;
-}
-
-and regex = Oniguruma.Encoding.utf8 Oniguruma.t
-
-and match_ = {
-  name : string option;
-  pattern : regex;
-  captures : capture IntMap.t;
-}
-
-and delim_kind = End | While
-
-and delim = {
-  delim_begin : regex;
-  delim_end : string; (* Either an end or a while pattern *)
-  delim_patterns : rule list;
-  delim_name : string option;
-  delim_content_name : string option;
-  delim_begin_captures : capture IntMap.t;
-  delim_end_captures : capture IntMap.t;
-  delim_apply_end_pattern_last : bool;
-  delim_kind : delim_kind;
-}
-
-and rule =
-  | Match of match_
-  | Delim of delim
-  | Include_local of string
-  | Include_scope of string
-  | Include_self
-  | Include_base
-
-type repo_item_kind =
-  | Repo_rule of rule
-  | Repo_patterns of rule list
-
-type repo_item = {
-  repo_item_kind : repo_item_kind;
-  repo_inner : (string, repo_item) Hashtbl.t;
-}
-
-type grammar = {
-  name : string;
-  scope_name : string;
-  patterns : rule list;
-  repository : (string, repo_item) Hashtbl.t;
-}
-
-type t = {
-  by_name : (string, grammar) Hashtbl.t;
-  by_scope_name : (string, grammar) Hashtbl.t;
-}
-
-type plist =
-  [ `Bool of bool
-  | `Data of string
-  | `Date of float * float option
-  | `Float of float
-  | `Int of int
-  | `String of string
-  | `Array of plist list
-  | `Dict of (string * plist) list
-  ]
-
-exception Error of string
-
-let create () = {
-  by_name = Hashtbl.create 23;
-  by_scope_name = Hashtbl.create 23;
-}
-
-let add_grammar t grammar =
-  Hashtbl.add t.by_name (String.lowercase_ascii grammar.name) grammar;
-  Hashtbl.add t.by_scope_name grammar.scope_name grammar
-
-let find_by_name t name =
-  Hashtbl.find_opt t.by_name (String.lowercase_ascii name)
-
-let find_by_scope_name t = Hashtbl.find_opt t.by_scope_name
-
-let error msg = raise (Error msg)
-
-let rec find key = function
-  | [] -> None
-  | (k, v) :: obj ->
-    if k = key then
-      Some v
-    else
-      find key obj
-
-let find_exn key obj =
-  match find key obj with
-  | Some v -> v
-  | None -> error (key ^ " not found.")
-
-let get_dict = function
-  | `Dict d -> d
-  | _ -> error "Type error: Expected dict."
-
-let get_string = function
-  | `String s -> s
-  | _ -> error "Type error: Expected string."
-
-let get_list f = function
-  | `Array l -> List.map f l
-  | _ -> error "Type error: Expected list."
-
-let of_plist_exn plist =
-  let compile_regex re =
-    match
-      Oniguruma.create
-        re Oniguruma.Options.none
-        Oniguruma.Encoding.utf8 Oniguruma.Syntax.default
-    with
-    | Error msg -> error (re ^ ": " ^ msg)
-    | Ok re -> re
-  in
-  let rec get_captures acc = function
-    | [] -> acc
-    | (k, v) :: kvs ->
-      let idx = match int_of_string_opt k with
-        | Some int -> int
-        | None -> error (k ^ " is not an integer.")
-      in
-      let v = get_dict v in
-      let capture_name = match find "name" v with
-        | None -> None
-        | Some name -> Some (get_string name)
-      in
-      let capture_patterns = match find "patterns" v with
-        | None -> []
-        | Some v -> get_pattern_list v
-      in
-      get_captures (IntMap.add idx { capture_name; capture_patterns } acc) kvs
-  and get_pattern_list l = get_list (fun x -> patterns_of_plist (get_dict x)) l
-  and get_patterns obj = find_exn "patterns" obj |> get_pattern_list
-  and patterns_of_plist obj =
-    match find "include" obj with
-    | Some s ->
-      begin match get_string s with
-        | "$base" -> Include_base
-        | "$self" -> Include_self
-        | s ->
-          let len = String.length s in
-          if len > 0 && s.[0] = '#' then
-            Include_local (String.sub s 1 (len - 1))
-          else
-            Include_scope s
-      end
-    | None ->
-      match find "match" obj, find "begin" obj with
-      | Some s, None ->
-        Match
-          { pattern = compile_regex (get_string s)
-          ; name = Option.map get_string (find "name" obj)
-          ; captures =
-              match find "captures" obj with
-              | None -> IntMap.empty
-              | Some value -> get_captures IntMap.empty (get_dict value) }
-      | None, Some b ->
-        let e, key, delim_kind = match find "end" obj, find "while" obj with
-          | Some e, None -> e, "endCaptures", End
-          | None, Some e -> e, "whileCaptures", While
-          | _, _ -> error "Begin patterns must either have an end or while."
-        in
-        let delim_begin_captures, delim_end_captures =
-          match find "captures" obj with
-          | Some value ->
-            let captures = get_captures IntMap.empty (get_dict value) in
-            captures, captures
-          | None ->
-            ( (match find "beginCaptures" obj with
-               | Some value -> get_captures IntMap.empty (get_dict value)
-               | None -> IntMap.empty)
-            , (match find key obj with
-               | Some value -> get_captures IntMap.empty (get_dict value)
-               | None -> IntMap.empty) )
-        in
-        Delim
-          { delim_begin = compile_regex (get_string b)
-          ; delim_end = get_string e
-          ; delim_patterns =
-              begin match find "patterns" obj with
-                | None -> []
-                | Some v -> get_pattern_list v
-              end
-          ; delim_name = Option.map get_string (find "name" obj)
-          ; delim_content_name =
-              Option.map get_string (find "contentName" obj)
-          ; delim_begin_captures
-          ; delim_end_captures
-          ; delim_apply_end_pattern_last =
-              begin match find "applyEndPatternLast" obj with
-                | Some (`Int 1) -> true
-                | _ -> false
-              end
-          ; delim_kind }
-      | _, _ -> error "Pattern must be match, begin/end, or begin/while."
-  in
-  let rec get_repo_item obj =
-    { repo_item_kind =
-        begin match find "match" obj, find "begin" obj with
-          | None, None -> Repo_patterns (get_patterns obj)
-          | _, _ -> Repo_rule (patterns_of_plist obj)
-        end
-    ; repo_inner =
-        begin match find "repository" obj with
-          | None -> Hashtbl.create 0
-          | Some obj -> get_repo obj
-        end }
-  and get_repo obj =
-    let hashtbl = Hashtbl.create 31 in
-    List.iter (fun (k, v) ->
-        let v = get_dict v in
-        let item = get_repo_item v in
-        Hashtbl.add hashtbl k item
-      ) (get_dict obj);
-    hashtbl
-  in
-  let obj = get_dict plist in
-  { name = get_string (find_exn "name" obj)
-  ; scope_name = get_string (find_exn "scopeName" obj)
-  ; patterns = get_patterns obj
-  ; repository =
-      match find "repository" obj with
-      | None -> Hashtbl.create 0
-      | Some obj -> get_repo obj }
+include Common
+include Reader
 
 type token = {
   ending : int;
@@ -264,68 +34,6 @@ let rec add_scopes scopes = function
 let next_pats grammar = function
   | [] -> grammar.patterns
   | s :: _ -> s.stack_delim.delim_patterns
-
-(* Emit tokens for the match region's captures. *)
-let handle_captures scopes default mat_start mat_end region captures tokens =
-  let rec new_scopes acc = function
-    | [] -> acc
-    | (_, scope) :: xs -> new_scopes (scope :: acc) xs
-  in
-  let _, stack, tokens =
-    (* Regex captures are ordered by their left parentheses. Do a depth-first
-       preorder traversal by keeping a stack of captures. *)
-    IntMap.fold (fun idx capture (start, stack, tokens) ->
-        (* If the capture mentions a lookahead, it can go past the bounds of
-           its parent. The match is capped at the boundary for the parent. Is
-           this the right decision to make? Clearly the writer of the grammar
-           intended for the capture to exceed the parent in this case. *)
-        if idx < 0 || idx >= Oniguruma.Region.length region then
-          (start, stack, tokens)
-        else
-          let cap_start = Oniguruma.Region.capture_beg region idx in
-          let cap_end = Oniguruma.Region.capture_end region idx in
-          if cap_start = -1 then
-            (* Capture wasn't found, ignore *)
-            (start, stack, tokens)
-          else
-            match stack with
-            | [] ->
-               let cap_start = if cap_start < start then start else cap_start in
-               let cap_end = if cap_end > mat_end then mat_end else cap_end in
-               ( cap_start
-               , [(cap_end, capture.capture_name)]
-               , { scopes = add_scopes scopes [default]; ending = cap_start }
-                 :: tokens )
-            | (top_end, top_name) :: stack' ->
-               let cap_start = if cap_start < start then start else cap_start in
-               if cap_start >= top_end then
-                 let under = match stack' with
-                   | [] -> mat_end
-                   | (end_, _) :: _ -> end_
-                 in
-                 let cap_end = if cap_end > under then under else cap_end in
-                 (* Pop off the stack, then push the capture *)
-                 ( top_end
-                 , (cap_end, capture.capture_name) :: stack'
-                 , { scopes = add_scopes scopes (new_scopes [top_name] stack')
-                   ; ending = cap_start } :: tokens )
-               else
-                 let cap_end = if cap_end > top_end then top_end else cap_end in
-                 (* Push the capture on the stack *)
-                 ( cap_start
-                 , (cap_end, capture.capture_name) :: stack
-                 , { scopes = add_scopes scopes (new_scopes [top_name] stack)
-                   ; ending = cap_start } :: tokens )
-      ) captures (mat_start, [], tokens)
-  in
-  let rec pop tokens = function
-    | [] -> tokens
-    | (ending, scope) :: stack ->
-      pop
-        ({ scopes = add_scopes scopes (new_scopes [scope] stack)
-         ; ending } :: tokens)
-        stack
-  in pop tokens stack
 
 (* Should the character be escaped in a regex? *)
 let is_special ch =
@@ -421,6 +129,79 @@ let remove_empties =
         go (tok :: acc) toks
   in go []
 
+(* Emit tokens for the match region's captures. *)
+let handle_captures
+    scopes default mat_start mat_end region captures tokens =
+  let rec new_scopes acc = function
+    | [] -> acc
+    | (_, scope) :: xs -> new_scopes (scope :: acc) xs
+  in
+  let _, stack, tokens =
+    (* Regex captures are ordered by their left parentheses. Do a depth-first
+       preorder traversal by keeping a stack of captures. *)
+    IntMap.fold (fun idx capture (start, stack, tokens) ->
+        (* If the capture mentions a lookahead, it can go past the bounds of
+           its parent. The match is capped at the boundary for the parent. Is
+           this the right decision to make? Clearly the writer of the grammar
+           intended for the capture to exceed the parent in this case. *)
+        if idx < 0 || idx >= Oniguruma.Region.length region then
+          (start, stack, tokens)
+        else
+          let cap_start = Oniguruma.Region.capture_beg region idx in
+          let cap_end = Oniguruma.Region.capture_end region idx in
+          if cap_start = -1 then
+            (* Capture wasn't found, ignore *)
+            (start, stack, tokens)
+          else
+            match stack with
+            | [] ->
+               let cap_start = if cap_start < start then start else cap_start in
+               let cap_end = if cap_end > mat_end then mat_end else cap_end in
+               let tokens =
+                 { scopes = add_scopes scopes [default]
+                 ; ending = cap_start }
+                 :: tokens
+               in
+               ( cap_start
+               , [(cap_end, capture.capture_name)]
+               , tokens )
+            | (top_end, top_name) :: stack' ->
+               let cap_start = if cap_start < start then start else cap_start in
+               if cap_start >= top_end then
+                 let under = match stack' with
+                   | [] -> mat_end
+                   | (end_, _) :: _ -> end_
+                 in
+                 let cap_end = if cap_end > under then under else cap_end in
+                 let tokens =
+                   { scopes = add_scopes scopes (new_scopes [top_name] stack')
+                   ; ending = cap_start } :: tokens
+                 in
+                 (* Pop off the stack, then push the capture *)
+                 ( top_end
+                 , (cap_end, capture.capture_name) :: stack'
+                 , tokens )
+               else
+                 let cap_end = if cap_end > top_end then top_end else cap_end in
+                 let tokens =
+                   { scopes = add_scopes scopes (new_scopes [top_name] stack)
+                   ; ending = cap_start } :: tokens
+                 in
+                 (* Push the capture on the stack *)
+                 ( cap_start
+                 , (cap_end, capture.capture_name) :: stack
+                 , tokens )
+      ) captures (mat_start, [], tokens)
+  in
+  let rec pop tokens = function
+    | [] -> tokens
+    | (ending, scope) :: stack ->
+      pop
+        ({ scopes = add_scopes scopes (new_scopes [scope] stack)
+         ; ending } :: tokens)
+        stack
+  in pop tokens stack
+
 (* Tokenizes a line according to the grammar.
 
    [t]: The collection of grammars.
@@ -434,13 +215,10 @@ let rec match_line ~t ~grammar ~stack ~pos ~toks ~line rem_pats =
   let len = String.length line in
   let scopes, stk_pats, repos, cur_grammar = match stack with
     | [] ->
-      [grammar.scope_name], grammar.patterns, [grammar.repository], grammar
+      ([grammar.scope_name], grammar.patterns, [grammar.repository], grammar)
     | se :: _ ->
       let d = se.stack_delim in
-      ( se.stack_scopes
-      , d.delim_patterns
-      , se.stack_repos
-      , se.stack_grammar )
+      (se.stack_scopes, d.delim_patterns, se.stack_repos, se.stack_grammar)
   in
   (* Try each pattern in the list until one matches. If none match, increment
      [pos] and try all the patterns again. *)
