dc1c9dbd080ad069b8f66f9f30487f3c40dde323
Alan Hu <alanh@ccs.neu.edu>
2021-07-11 18:19:16 -0400

    Use built-in List.assoc_opt function

--- src/reader.ml
+++ src/reader.ml
@@ -1,15 +1,7 @@
 open Common
 
-let rec find key = function
-  | [] -> None
-  | (k, v) :: obj ->
-    if k = key then
-      Some v
-    else
-      find key obj
-
 let find_exn key obj =
-  match find key obj with
+  match List.assoc_opt key obj with
   | Some v -> v
   | None -> error (key ^ " not found.")
 
@@ -42,11 +34,11 @@ let rec get_captures acc = function
       | None -> error (k ^ " is not an integer.")
     in
     let v = get_dict v in
-    let capture_name = match find "name" v with
+    let capture_name = match List.assoc_opt "name" v with
       | None -> None
       | Some name -> Some (get_string name)
     in
-    let capture_patterns = match find "patterns" v with
+    let capture_patterns = match List.assoc_opt "patterns" v with
       | None -> []
       | Some v -> get_pattern_list v
     in
@@ -54,7 +46,7 @@ let rec get_captures acc = function
 and get_pattern_list l = get_list (fun x -> patterns_of_plist (get_dict x)) l
 and get_patterns obj = find_exn "patterns" obj |> get_pattern_list
 and patterns_of_plist obj =
-  match find "include" obj with
+  match List.assoc_opt "include" obj with
   | Some s ->
     begin match get_string s with
       | "$base" -> Include_base
@@ -67,31 +59,32 @@ and patterns_of_plist obj =
           Include_scope s
     end
   | None ->
-    match find "match" obj, find "begin" obj with
+    match List.assoc_opt "match" obj, List.assoc_opt "begin" obj with
     | Some s, None ->
       Match
         { pattern = compile_regex (get_string s)
-        ; name = Option.map get_string (find "name" obj)
+        ; name = Option.map get_string (List.assoc_opt "name" obj)
         ; captures =
-            match find "captures" obj with
+            match List.assoc_opt "captures" obj with
             | None -> IntMap.empty
             | Some value -> get_captures IntMap.empty (get_dict value) }
     | None, Some b ->
-      let e, key, delim_kind = match find "end" obj, find "while" obj with
+      let e, key, delim_kind =
+        match List.assoc_opt "end" obj, List.assoc_opt "while" obj with
         | Some e, None -> e, "endCaptures", End
         | None, Some e -> e, "whileCaptures", While
         | _, _ -> error "Begin patterns must either have an end or while."
       in
       let delim_begin_captures, delim_end_captures =
-        match find "captures" obj with
+        match List.assoc_opt "captures" obj with
         | Some value ->
           let captures = get_captures IntMap.empty (get_dict value) in
           captures, captures
         | None ->
-          ( (match find "beginCaptures" obj with
+          ( (match List.assoc_opt "beginCaptures" obj with
              | Some value -> get_captures IntMap.empty (get_dict value)
              | None -> IntMap.empty)
-          , (match find key obj with
+          , (match List.assoc_opt key obj with
              | Some value -> get_captures IntMap.empty (get_dict value)
              | None -> IntMap.empty) )
       in
@@ -99,16 +92,17 @@ and patterns_of_plist obj =
         { delim_begin = compile_regex (get_string b)
         ; delim_end = get_string e
         ; delim_patterns =
-            begin match find "patterns" obj with
+            begin match List.assoc_opt "patterns" obj with
               | None -> []
               | Some v -> get_pattern_list v
             end
-        ; delim_name = Option.map get_string (find "name" obj)
-        ; delim_content_name = Option.map get_string (find "contentName" obj)
+        ; delim_name = Option.map get_string (List.assoc_opt "name" obj)
+        ; delim_content_name =
+            Option.map get_string (List.assoc_opt "contentName" obj)
         ; delim_begin_captures
         ; delim_end_captures
         ; delim_apply_end_pattern_last =
-            begin match find "applyEndPatternLast" obj with
+            begin match List.assoc_opt "applyEndPatternLast" obj with
               | Some (`Int 1) -> true
               | _ -> false
             end
@@ -118,12 +112,12 @@ and patterns_of_plist obj =
 let of_doc_exn (plist : union) =
   let rec get_repo_item obj =
     { repo_item_kind =
-        begin match find "match" obj, find "begin" obj with
+        begin match List.assoc_opt "match" obj, List.assoc_opt "begin" obj with
           | None, None -> Repo_patterns (get_patterns obj)
           | _, _ -> Repo_rule (patterns_of_plist obj)
         end
     ; repo_inner =
-        begin match find "repository" obj with
+        begin match List.assoc_opt "repository" obj with
           | None -> Hashtbl.create 0
           | Some obj -> get_repo obj
         end }
@@ -141,7 +135,7 @@ let of_doc_exn (plist : union) =
   ; scope_name = get_string (find_exn "scopeName" obj)
   ; patterns = get_patterns obj
   ; repository =
-      match find "repository" obj with
+      match List.assoc_opt "repository" obj with
       | None -> Hashtbl.create 0
       | Some obj -> get_repo obj }
 
